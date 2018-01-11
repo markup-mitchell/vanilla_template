3fc20f0affe88c7a156f04e341c94cabcc18ac46
spitchell <mr.spitchell@gmail.com>
2018-01-11 17:22:57 +0000

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("js is working");
+// alert("js is working");
+//
+
+const position = [0, 90];
+const imageStyle = document.querySelector('#image').style;
+
+function frame1() {
+  imageStyle.right = `${position[0]}`;
+}
+
+function frame2() {
+  imageStyle.right = `${position[1]}%`;
+}
